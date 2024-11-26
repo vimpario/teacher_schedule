bef632ec915336a22b16579a54f43d100ad6c8b5
vimpario <koz.viktor2020@gmail.com>
2024-11-26 20:02:29 +0300

    simple API template

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"teacher_schedule/internal/schedule"
-	"teacher_schedule/internal/users"
+	"log"
+	"net/http"
 	"teacher_schedule/pkg/config"
-	"teacher_schedule/pkg/db"
+	
+	"teacher_schedule/pkg/httputil"
 )
 
 func main() {
     config.LoadConfig()
 
-    connectionString := "host=" + config.GetEnv("DB_HOST") +
-        " user=" + config.GetEnv("DB_USER") +
-        " password=" + config.GetEnv("DB_PASSWORD") +
-        " dbname=" + config.GetEnv("DB_NAME") +
-        " port=" + config.GetEnv("DB_PORT") +
-        " sslmode=disable"
-    database := db.Connect(connectionString)
-
-    database.AutoMigrate(&users.User{}, &schedule.Schedule{})
-	
+    router := httputil.NewRouter()
+    log.Println("Сервер запущен на порту 8080....")
+    http.ListenAndServe(":8080", router)
 }
---
+++ pkg/httputil/router.go
@@ -0,0 +1,16 @@
+package httputil
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter() *chi.Mux{
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("АПИ Расписание Преподавателя"))
+	})
+	return r
+}
