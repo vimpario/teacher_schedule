53c01351409bef4ad1563d8580f94bf49192c5d2
vimpario <koz.viktor2020@gmail.com>
2024-11-26 21:21:05 +0300

    schedule model binding corrects

--- internal/schedule/schedule_model.go
+++ internal/schedule/schedule_model.go
@@ -1,11 +1,12 @@
 package schedule
 
 type Schedule struct {
-	ID         uint  `gorm:"primaryKey"`
-	TeacherID  uint  `gorm:"not null"`
-	DayID      uint  `gorm:"not null"`
-	SlotID     uint  `gorm:"not null"`
-	GroupID    *uint `gorm:""`
-	SubjectID  *uint `gorm:""`
-	IsOccupied bool  `gorm:"default:false"`
+	ID         uint  `gorm:"column:scheduleid"`
+	TeacherID  uint  `gorm:"column:teacherid;not null"`
+	DayID      uint  `gorm:"column:dayid;not null"`
+	SlotID     uint  `gorm:"column:slotid;not null"`
+	GroupID    *uint `gorm:"column:groupid"`
+	SubjectID  *uint `gorm:"column:subjectid"`
+	IsOccupied bool  `gorm:"column:isoccupied;default:false"`
+	ClassroomID uint `gorm:"column:classroomid"`
 }
--- internal/users/user_model.go
+++ internal/users/user_model.go
@@ -2,8 +2,8 @@ package users
 
 type User struct {
 	ID         uint   `gorm:"column:userid"`
-	FirstName  string `grom:"column:firstname;size:50;not null"`
-	LastName   string `grom:"column:lastname;size:50;not null"`
+	FirstName  string `grom:"column:first_name;size:50;not null"`
+	LastName   string `grom:"column:last_name;size:50;not null"`
 	Patronymic string `grom:"column:patronymic;size:50"`
 	BirthDate  string `gorm:"column:birthdate;not null"`
 	RoleID     uint   `gorm:"column:roleid;not null"`
